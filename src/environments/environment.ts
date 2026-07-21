9fdce2dab5d7c6ff6b6f965a2eba61245630f7ff
Johan Alexis Contreras Granados <126220172+johancg04@users.noreply.github.com>
2026-07-20 19:31:50 -0500

    Remove API keys from environment.ts
    Removed sensitive API keys from environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   platformProviderItemsEndpointPath: '/flight-items',
   logoProviderBaseUrl: 'https://img.logo.dev',
   aviationStackBaseUrl: 'http://api.aviationstack.com/v1',
-  aviationStackKey: '071a93b5264526665073b6bdf098b5fa',
-  logoDevKey: 'pk_I1p5ChnQTVemRtFb_JsUHw'
+  aviationStackKey: '',
+  logoDevKey: ''
 };
